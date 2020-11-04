7a8155dc1b8145e21da11f3554321500494f092a
cadena87 <cadena87@hotmail.com>
2020-11-03 23:11:10 -0300

    Deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path')
 const app = express;
-app.use(express.static(__dirname + '/dist/tasklist'));
+app.use(express.static(__dirname + '/dist/Tasklist'));
 app.getActiveAttrib('/*', function (req, resp) {
-  res.sendFile(path.join(__dirname+'/dist/tasklist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/Tasklist/index.html'));
 });
 app.listen(process.env.PORT || 8080);
