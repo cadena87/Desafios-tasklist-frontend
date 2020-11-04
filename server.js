f86e5c212eaaee2b5718c8c60acfd6a8d2c23721
cadena87 <cadena87@hotmail.com>
2020-11-03 23:49:47 -0300

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path')
 const app = express;
+
+const PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/dist/Tasklist'));
-app.getActiveAttrib('/*', function (req, resp) {
+
+app.get('/*', function (req, resp) {
   res.sendFile(path.join(__dirname+'/dist/Tasklist/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+app.listen(PORT, () => {
+  console.log('Servidor iniciado na porta'+PORT);
+});
