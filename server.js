7ad6fccd68ab9dc47046de4eb225e734cd4c47b6
cadena87 <cadena87@hotmail.com>
2020-11-03 23:59:12 -0300

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path')
-const app = express;
+const app = express();
 
 const PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/dist/Tasklist'));
